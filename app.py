91d482632d52179376ff6136b83dde16d058394b
JuanVargas18 <juan18031205@gmail.com>
2022-11-17 13:04:58 -0300

    Damos funcionalidad

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
+from markupsafe import escape
 app = Flask(__name__)
 
 @app.get("/")
 def hola_mundo():
-    return render_template("saludo.html", )
+    return render_template("index.html", )
 
     
-@app.get("/adios")
-def adios():
-    return f"""
-    <h1>¡Hola {request.args.get("nombre", "mundo")}!</h1>"
-    <p>
-    Esta es nuesta <strong> primera </strong> página
-    </p>
-    """
+@app.get("/bienvenida")
+def bienvenida():
+    return render_template("bienvenida.html", nombre=nombre())
 
-print("Iniciando servidor")
+def nombre():
+    return request.args.get("nombre", "mundo")
